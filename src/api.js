98b92bf0f738d6d8a2dd1f285a192f09518f9253
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2022-07-06 12:20:11 +0200

    token error fix

--- src/api.js
+++ src/api.js
@@ -57,8 +57,8 @@ import NProgress from 'nprogress';
   export const getAccessToken = async () => {
     const accessToken = localStorage.getItem('access_token');
     const tokenCheck = accessToken && (await checkToken(accessToken));
-
-    if (!accessToken || tokenCheck.error) {
+// || tokenCheck.error)
+    if (!accessToken  ){
       await localStorage.removeItem("access_token");
       const searchParams = new URLSearchParams(window.location.search);
       const code = await searchParams.get("code");
