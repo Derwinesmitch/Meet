21294e74d7def64c29ea2d17341398595c656926
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2022-08-09 08:52:16 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Meet/precache-manifest.7e1729a7da0af3b9bc3212ea5dc833a9.js"
+  "/Meet/precache-manifest.0ceeb65265313b0e0877a682341aa7c3.js"
 );
 
 workbox.clientsClaim();
