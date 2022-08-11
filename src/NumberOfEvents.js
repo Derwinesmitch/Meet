c0f493ca25739de47ea89339633a75a86e9491ac
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2022-08-11 12:13:13 +0200

    fixing some code

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -3,39 +3,39 @@ import { ErrorAlert } from './alert';
 
 class NumberOfEvents extends Component {
   state = {
-    eventNumbers: 32,
+    // eventNumbers: 32,
     infoText: '',
   }
 
   handleInputChanged = (event) => {
-    let inputValue = event.target.value;
-    if (inputValue >= 33 || inputValue <= 0) {
+    // let inputValue = event.target.value;
+    const inputValue = parseInt(event.target.value);
+    if (inputValue > 0 || inputValue < 33) {
       this.setState({
         eventNumbers: inputValue,
-        infoText: 'enter valid number between 1 and 32',
+        infoText: '',
       });
     } else {
       this.setState({
-        eventNumebers: event.target.value,
-        infoText: '',
+        eventNumbers: 32,
+        infoText: 'enter valid number between 1 and 32',
       });
     }
-
-        this.props.updateEvents(undefined, inputValue);
+    
+    this.props.updateEvents(undefined, inputValue);
     }
 
   render() {
-    return (
-      <div className="eventNumbers">
-        <ErrorAlert text={this.state.infoText} />
-        <input
-          className="eventNumbersList"
-          type="number"
-          value={this.state.eventNumbers}
-          onChange={this.handleInputChanged}
-          />
+    const { eventNumbers } = this.props;
+    return <div className="eventNumbers">
+      
+      
+      <label>
+            <input className="eventNumbersList" type="number" value={eventNumbers} onChange={this.handleInputChanged}> 
+            </input>
+          </label>
+          <ErrorAlert text={this.state.infoText} />
       </div>
-    );
   }
 }
 
