30c330229a5e72e0e7a9e4766fe895c746a10032
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2022-08-11 12:28:43 +0200

    Updates

--- precache-manifest.01d19524fb4eb24dd29ae4db0ec5618b.js
+++ precache-manifest.01d19524fb4eb24dd29ae4db0ec5618b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "209ae8a3f9fd3a2523aa",
+    "revision": "a8acfe2dd8c3fcae7606",
     "url": "/Meet/static/css/main.3cdc5e4b.chunk.css"
   },
   {
-    "revision": "209ae8a3f9fd3a2523aa",
-    "url": "/Meet/static/js/main.fb4f0e04.chunk.js"
+    "revision": "a8acfe2dd8c3fcae7606",
+    "url": "/Meet/static/js/main.6947f50e.chunk.js"
   },
   {
     "revision": "5e534f4b80f06b8d3ebd",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/Meet/static/js/3.b6082a70.chunk.js"
   },
   {
-    "revision": "047740dea04a6a30aabab2eeeea27a0c",
+    "revision": "295da583d61c7033666e31d7356ec311",
     "url": "/Meet/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Meet/precache-manifest.b6852316bb55b9822d341f5a0220a952.js"
+  "/Meet/precache-manifest.01d19524fb4eb24dd29ae4db0ec5618b.js"
 );
 
 workbox.clientsClaim();
